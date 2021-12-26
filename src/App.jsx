17dd6b96f41a4b408331982e0cbb31cd6fae5684
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2021-12-26 15:31:37 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,20 @@
-import React, { Component } from 'react';
-import './app.css';
-import axios from 'axios';
+import React from "react";
 
-class App extends Component {
-  constructor (props) {
-    super(props);
+class App extends React.Component {
+  constructor () {
+    super();
     this.state = {
-      posts: []
+
     };
   }
-  componentDidMount() {
-    axios.get('https://randomuser.me/api/?results=100')
-      .then(response => {
-        console.log(response.data.results);
-        this.setState({
-          posts: response.data.results
-        });
-      });
-  }
 
-  render() {
-    const { posts } = this.state;
-    const postList = posts.length ? (
-      posts.map(post => {
-        console.log(post.name.first);
-
-        return (
-          <div className='card' key={post.login.uuid}>
-            <div>
-              <img className='image' alt={post.login.username} src={post.picture.large} />
-            </div>
-            <h3>{post.name.title}, {post.name.first} {post.name.last}</h3>
-            <h5>Gender: {post.gender}</h5>
-            <h5>Age: {post.dob.age}</h5>
-            <h5>City :{post.location.city}</h5>
-            <h5>Street :{post.location.street.name}</h5>
-            <h5>Phone :{post.phone}</h5>
-          </div>
-        )
-      })
-    ) : (
-      <div className="center">No posts yet</div>
-    )
+  render = () => {
     return (
-      <div wrapper>
-        <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname"></input>
-        <div className='container'>
-          {postList}
-        </div>
+      <div className="App">
+        hello world
       </div>
-
     );
-  }
+  };
 }
 
 export default App;
